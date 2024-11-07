07478ee3e288450d89b06742b01aec194d8e9229
sreehb-123 <sreeh190206@gmail.com>
2024-11-07 12:23:05 +0530

    font++

--- src/components/Strips.js
+++ src/components/Strips.js
@@ -7,18 +7,14 @@ const Strips = () => {
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus sapien eget enim facilisis 
                     vulputate. Integer volutpat sem eget lorem convallis, non suscipit libero condimentum. Phasellus sit 
-                    amet arcu nec nulla ultricies gravida. Duis ac eros vel eros efficitur tempus. Suspendisse potenti. 
-                    Vestibulum cursus diam vel suscipit tincidunt. Integer nec augue sit amet lectus tempus gravida a sit 
-                    amet eros. Aenean euismod ligula nec elit fringilla, eu malesuada velit elementum.
+                    amet arcu nec nulla ultricies gravida.
                 </p>
             </div>
             <div className="strip strip-right">
                 <p>
                     Praesent vestibulum lorem et massa vestibulum, vel dictum lacus fermentum. Ut malesuada arcu ac nibh 
                     scelerisque, id vehicula justo fringilla. Duis eu elit fermentum, fermentum odio et, scelerisque augue. 
-                    Aliquam erat volutpat. Vivamus blandit nulla sed erat dignissim, sed efficitur ante congue. Cras non 
-                    tellus nec libero volutpat tincidunt. Pellentesque habitant morbi tristique senectus et netus et 
-                    malesuada fames ac turpis egestas. Nullam sit amet metus vehicula, porttitor dui eu, pellentesque risus.
+                    Aliquam erat volutpat. Vivamus blandit nulla sed erat dignissim, sed efficitur ante.
                 </p>            
             </div>
             <div className="strip strip-left">
@@ -26,8 +22,7 @@ const Strips = () => {
                     Fusce vehicula eu lectus id fringilla. Donec at nisl orci. Aliquam erat volutpat. Nullam rhoncus 
                     ullamcorper varius. Phasellus lacinia sem vel nisl vehicula, id interdum magna dapibus. Curabitur 
                     ullamcorper, mauris vel vulputate aliquet, erat lorem dapibus purus, sit amet feugiat eros est nec 
-                    sapien. Quisque non risus ut velit sagittis vestibulum vel et felis. Cras sollicitudin risus a risus 
-                    varius, id laoreet purus feugiat. Maecenas vitae ipsum id arcu finibus dignissim.
+                    sapien.
                 </p>
             </div>
         </div>
